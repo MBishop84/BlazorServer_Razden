3b30904287d06519b8d05ef2d9b6b721e4d72624
Mitch Bishop <Mitch.Bishop@NGIC.COM>
2023-09-27 14:07:14 -0400

    Using NASA api.

---
+++ Transformer+/Data/NASA/Exoplanet.cs
@@ -0,0 +1,16 @@
+﻿namespace Transformer_.Data.NASA
+{
+    /// <summary>
+    /// An exoplanet Object
+    /// </summary>
+    public class Exoplanet
+    {
+        public string pl_name { get; set; }
+        public int disc_year { get; set; }
+        public string discoverymethod { get; set; }
+        public string hostname { get; set; }
+        public string disc_facility { get; set; }
+        public string disc_instrument { get; set; }
+        public string pl_orbper_reflink { get; set; }
+    }
+}
